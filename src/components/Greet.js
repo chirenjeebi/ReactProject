40c3391387dd0d1ba835926edd639a15d2b0db15
chirenjeebi <cparajuli100@outlook.com>
2019-12-19 22:33:07 -0500

    first react project commited

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { func } from 'prop-types'
+import App from '../App'
+
+
+// function Greet(){
+//     return <h1>Hello Vishwas</h1>
+// }
+
+ const Greet=()=><h1>Hello Vishwas</h1>
+
+ export default Greet;
